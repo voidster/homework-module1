4c44da46c2050a8b6d2bfe2ea95ac35e4bacfaf8
voidster <eszettv@icloud.com>
2025-02-02 18:45:08 -0500

    wk4 d2 commit 2

--- js/randomcodes.js
+++ js/randomcodes.js
@@ -4,7 +4,8 @@ function generateCode() {
   var code = " "; // initialize to null value
   var str = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789@#$";
 
-  // Generate character multiple times using a loop
+  var btnvalue; // for button boolean value
+  var getCode = " ";
   // Generate character multiple times using a loop
   for (i = 1; i <= 8; i++) {
     var char = Math.random() * str.length; // random select a character from the variable
@@ -17,10 +18,5 @@ function generateCode() {
 // Get HTML element to display
 document.getElementById("codes").innerHTML = generateCode();
 
-// Disable Button
-function disableButton() {
-  document.getElementById("submit").disabled = true;
-}
-
 // Activate function
-disableButton();
+// disableButton();
